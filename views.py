fc2781e67f9b49ba9fd1d3801116e9b8bdf05112
wlorenzetti <lorenzetti@gis3w.it>
2020-07-13 08:11:23 +0200

    Add first view: elevation profile projects view.

--- views.py
+++ views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+# coding=utf-8
+"""Eleprofile views module.
 
-# Create your views here.
+.. note:: This program is free software; you can redistribute it and/or modify
+    it under the terms of the Mozilla Public License 2.0.
+
+"""
+
+__author__ = 'lorenzetti@gis3w.it'
+__date__ = '2020-07-09'
+__copyright__ = 'Copyright 2015 - 2020, Gis3w'
+
+
+from django.views.generic import ListView
+from .models import EleProProject
+
+
+class ProjectsListView(ListView):
+    """List projecta view."""
+    template_name = 'eleprofile/projects_list.html'
+    model = EleProProject
