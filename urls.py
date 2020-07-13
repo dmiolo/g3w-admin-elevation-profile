c552eeac37dea13c9a0ee3a50d0ff2eb4dce818b
wlorenzetti <lorenzetti@gis3w.it>
2020-07-13 08:10:52 +0200

    Add first url: project elevation profile list.

--- urls.py
+++ urls.py
@@ -10,4 +10,14 @@ __author__ = 'lorenzetti@gis3w.it'
 __date__ = '2020-07-09'
 __copyright__ = 'Copyright 2015 - 2020, Gis3w'
 
-urlpatterns = []
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+from base.urls import G3W_SITETREE_I18N_ALIAS
+from .views import ProjectsListView
+
+# For sitree bar translation
+G3W_SITETREE_I18N_ALIAS.append('eleprofile')
+
+urlpatterns = [
+    path('projects/', login_required(ProjectsListView.as_view()), name='eleprofile-porject-list')
+]
